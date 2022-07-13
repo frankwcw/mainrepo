a3e97c46c177f6c44a55a5b310948b2690a364a3
frankwcw <enbewuwk@gmail.com>
2022-07-13 16:01:34 +0800

    Update

--- practice/node/esbuild-template/esbuild-default/build/index.js
+++ practice/node/esbuild-template/esbuild-default/build/index.js
@@ -1,9 +1,15 @@
 const { build } = require('esbuild')
+const { getCommandArgs } = require('./lib/get-command-args')
+
+const { sourcemap } = getCommandArgs({
+	sourcemap: false,
+})
 
 build({
 	entryPoints: ['./src/core/main.js'],
 	outfile: './dist/main.js',
 	minify: true,
 	bundle: true,
+	sourcemap,
 	platform: 'node',
 }).catch(() => process.exit(1))
--- practice/node/esbuild-template/esbuild-default/src/core/main.js
+++ practice/node/esbuild-template/esbuild-default/src/core/main.js
@@ -2,5 +2,6 @@ import { initEnvConfig, env } from './lib/env'
 
 !(function setup() {
 	if (!initEnvConfig()) return
+	const a = 123
 	console.log(env)
 })()
