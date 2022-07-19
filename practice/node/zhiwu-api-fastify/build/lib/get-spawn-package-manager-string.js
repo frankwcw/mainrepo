40894cb2d28da426cd6e492ed9cde0bdaae8af52
frankwcw <enbewuwk@gmail.com>
2022-07-19 09:58:20 +0800

    Refactor: 移除用不到的 lib

--- practice/node/zhiwu-api-fastify/build/lib/get-spawn-package-manager-string.js
+++
@@ -1,6 +0,0 @@
-const getSpawnPackageManagerString = (packageManagerName = 'pnpm') =>
-	/^win/.test(process.platform)
-		? `${packageManagerName}.cmd`
-		: packageManagerName
-
-module.exports = { getSpawnPackageManagerString }
