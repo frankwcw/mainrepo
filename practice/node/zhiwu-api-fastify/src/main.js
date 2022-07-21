5bd02106bbd9acbae28725fc0405ea8c5f42ee87
frankwcw <enbewuwk@gmail.com>
2022-07-21 13:07:37 +0800

    Feat: 添加自訂 logger 模板代碼

--- practice/node/zhiwu-api-fastify/src/main.js
+++ practice/node/zhiwu-api-fastify/src/main.js
@@ -3,8 +3,35 @@ import { env, setupEnv } from './lib/env'
 import { setupMongoose } from './lib/setup-mongoose'
 import { setupPlugins } from './lib/setup-plugins'
 
+function Logger(...args) {
+	this.args = args
+}
+Logger.prototype.info = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.error = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.debug = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.fatal = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.warn = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.trace = function (msg) {
+	console.log('myLogger', msg)
+}
+Logger.prototype.child = function () {
+	return new Logger()
+}
+
+const myLogger = new Logger()
+
 const fastify = Fastify({
-	logger: true,
+	logger: myLogger,
 })
 
 const log = msg => fastify.log.info(msg)
